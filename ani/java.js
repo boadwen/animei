8b94ff7a77506b79c47fbda81359cbccc93deb5c
boadwen <149164376+boadwen@users.noreply.github.com>
2024-02-20 13:08:39 +0000

    Add files via upload

---
+++ ani/java.js
@@ -0,0 +1,16 @@
+function sayhi(){
+   let username = prompt('what your favoutite anime?')
+    if(username === 'boadwen'){
+        console.log('welcome sir')
+        let you = prompt('what are you going to watch')
+        if(you === 'one piece'){
+            console.log('magnificenet choice')
+        }else{
+        console.log('really')
+        }
+    }else if(username == username){
+        console.log('i know who are you')
+    }else{
+        console.log('who are you!!!!')
+    }
+}
